aeef3615e04868e46b4d848b9b5327b2c4272a3c
Stibo <sl@stibo.ch>
2014-02-09 16:31:34 +0100

    #global: functions.rb moved to /responslr/sass/ and new config.rb

--- config.rb
+++ config.rb
@@ -1,5 +1,8 @@
-# Require any additional compass plugins here.
-require File.join(File.dirname(__FILE__), 'functions.rb')
+# responslr path
+responslr_path = "responslr/sass/"
+
+# Required responslr functions
+require File.join(File.dirname(__FILE__), responslr_path + 'functions.rb')
 
 # Set this to the root of your project when deployed:
 http_path = "/"
@@ -7,6 +10,8 @@ css_dir = "css"
 sass_dir = "sass"
 images_dir = "img"
 javascripts_dir = "js"
+
+add_import_path responslr_path
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
@@ -20,7 +25,6 @@ relative_assets = true
 # line_comments = false
 line_comments = false
 
-Sass::Script::Number.precision = 12
 # If you prefer the indented syntax, you might want to regenerate this
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
